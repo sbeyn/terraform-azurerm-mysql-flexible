44d44ac994040d8e609e1dba793ab5588578397e
sbeyn <sbeyn@users.noreply.github.com>
2023-07-23 23:00:51 +0200

    fix: add features: prevent_deletion_if_contains_resources = false.

--- examples/default/providers.tf
+++ examples/default/providers.tf
@@ -1,4 +1,9 @@
 provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   client_id       = var.client_id
   client_secret   = var.client_secret
   subscription_id = var.subscription_id
